b804ad6ebea79751bf4f741c2b579f95400add21
JetsadaWijit <jetsadawijit@outlook.com>
2025-08-17 06:45:29 +0000

    refactor: Set up new format for project sturcutre

--- mcengine-achievement-api/src/main/java/io/github/mcengine/api/achievement/database/IMCEngineAchievementApiDatabase.java
+++
@@ -1,6 +0,0 @@
-package io.github.mcengine.api.achievement.database;
-
-/**
- * Interface for AI API database operations related to storing and retrieving user tokens.
- */
-public interface IMCEngineAchievementApiDatabase {}
